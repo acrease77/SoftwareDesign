ab0fd0801c7d4991e1179b250f57db970540e3f5
Alexander Crease <alexander.crease@students.olin.edu>
2013-09-24 10:51:20 -0400

    adding my grid program

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,18 @@
 """Solution to an exercise in Think Python.
 
-Author:
+Author: Alexander Crease
 """
+def grid_print(x,y):
+	print ' + - - - -'*x,'+'
+	print ' |        '*x,'|'
+	print ' |        '*x,'|'
+	print ' |        '*x,'|'
+	print ' |        '*x,'|'
+	if y-1<=0:
+		print ' + - - - -'*x,'+'
+	else:
+		grid_print(x,y-1)
+		
+grid_print(2,2)
 
-print '+ - - - -'
+grid_print(4,4)
